ab24cfed782c822069cb25da29301e40ca38861d
Badru <mohbadrujaman9@gmail.com>
2022-04-14 00:05:07 +0700

    portfolio-tailwindcss

---
+++ src/script.js
@@ -0,0 +1,23 @@
+// Navbar Fixed
+
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    header.classList.remove("header-absolute");
+  } else {
+    header.classList.remove("navbar-fixed");
+    header.classList.add("header-absolute");
+  }
+};
+
+// Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#06b6d4",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      fontFamily: {
+        poppins: "Poppins",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
